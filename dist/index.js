5a1bab29af8ae19e0da0692a0c0294cf61b983b0
Rodrigo Vieira <rodrigosv91@gmail.com>
2022-02-28 03:27:52 -0300

    add: basic types

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,56 @@
 "use strict";
-//regular js
-//let id = 5   
-//ts
+let id_user = 5;
+console.log('ID:', id_user);
 let id = 5;
-id = '5';
+let company = 'My IT company';
+let isPublished = true;
+let x = 'Hello';
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, 'Hello'];
+let arrStudent;
+arrStudent = ['Ken', 1];
+let person = [1, 'jon', true];
+let employee;
+employee = [
+    [1, 'Peter'],
+    [2, 'Brad'],
+    [3, 'Jill']
+];
+let p_id = 22;
+p_id = 22;
+var Level;
+(function (Level) {
+    Level[Level["Low"] = 1] = "Low";
+    Level[Level["Medium"] = 2] = "Medium";
+    Level[Level["High"] = 3] = "High";
+})(Level || (Level = {}));
+let lv = Level.Medium;
+var Level2;
+(function (Level2) {
+    Level2["Low"] = "Low";
+    Level2["Medium"] = "Medium";
+    Level2["High"] = "High";
+})(Level2 || (Level2 = {}));
+console.log(lv);
+const user = {
+    id: 1,
+    name: 'John'
+};
+const user2 = {
+    id: 2,
+    name: 'Mike'
+};
+const user3 = {
+    id: 3,
+    name: 'Feige',
+    status: true
+};
+let cid = 1;
+let customerId = cid;
+let customerId2 = cid;
+function addNum(x, y) {
+    return x + y;
+}
+function log(message) {
+    console.log(message);
+}
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,94 @@
 //let id = 5   
 
 //ts
-let id: number = 5   
-id = '5'
+let id_user: number = 5   
+//id = '5'          // error
+
+console.log('ID:', id_user)
+
+//Basic Types
+let id: number = 5  
+let company: string = 'My IT company'
+let isPublished: boolean = true
+let x: any = 'Hello'
+
+//let ids = [1,2,3,4,5]             
+let ids: number[] = [1,2,3,4,5]     //specified array of numbers
+//ids.push('Hello') // error
+
+
+//Tuples
+let arr: any[] = [1, true, 'Hello']   
+
+let arrStudent: [string, number]   //array with a fixed number of elements whose types are known
+arrStudent = ['Ken', 1]
+
+let person: [number, string, boolean] = [1, 'jon', true]    //same spots
+
+//Tuple Array
+let employee: [number, string][]
+
+employee = [
+    [1, 'Peter'],
+    [2, 'Brad'],
+    [3, 'Jill']
+]
+
+//Union
+let p_id: string | number = 22  // one or another allowed
+p_id = 22 // or  p_id = '22'
+
+//Enum
+enum Level {       //set of named constants (as java)
+   Low = 1,        // start at 1 next 2,3 ... | default start: 0
+   Medium,
+   High
+}
+let lv: Level = Level.Medium    //let lv2: string = Level[2]    // gets the enum name at positon 2
+
+enum Level2 {      
+    Low = 'Low',        
+    Medium = 'Medium',
+    High = 'High'
+ }
+
+console.log(lv)     // console.log(Level.Medium) // console.log(Level[2])
+
+// Objects
+const user = {      //modo 1
+    id:1,
+    name: 'John'
+}
+
+const user2: {      //modo 2
+    id: number,
+    name: string
+} = {
+    id: 2,
+    name: 'Mike'
+}
+
+type User3 = {      //modo 3
+    id: number,
+    name: string,
+    status: boolean
+} 
+const user3: User3 = {  //use created type 'user3'
+    id: 3,
+    name: 'Feige',
+    status: true
+}
+
+// Type Assertion
+let cid: any = 1       
+let customerId = <number>cid         // modo 1
+let customerId2 = cid as number      // modo 2
+
+// Functions
+function addNum(x: number, y: number): number{      // fix x has an 'any' type error option 1: allow noImplicitAny
+    return x + y                                    //option 2: assign type
+}
+
+function log(message: string | number): void{       //void
+   console.log(message)
+}
